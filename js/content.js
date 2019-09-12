45214f27ebc17d857cbb58de43385c304bf2296e
kartinmat <martinkat@freemail.hu>
2019-09-12 14:04:00 +0200

    a

--- js/content.js
+++ js/content.js
@@ -10,7 +10,16 @@ class Content {
             return;
         }
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<!DOCTYPE html>");
+        res.write("<html lang = 'hu'>");
+        res.write("<head>");
+        res.write("<title>TeglalapTK</title>");
+        9;
+        res.write("</head>");
+        res.write("<body>");
         res.write("<h1>Téglalap területe és kerülete</h1>");
+        res.write("</body>");
+        res.write("</html>");
     }
 }
 exports.default = Content;
